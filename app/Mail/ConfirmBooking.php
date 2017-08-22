d7857a732d988e29374d48631fad1270e131989f
Jakob Lindved Marker <jakob@lindvedmarker.com>
2017-08-22 19:24:50 +0200

    Cleaned up directory.

--- app/Mail/ConfirmBooking.php
+++ app/Mail/ConfirmBooking.php
@@ -28,6 +28,6 @@ class ConfirmBooking extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('emails.confirm');
     }
 }
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,38 @@
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<meta name="description" content="Copenhagen Live {{ date("Y") }} on VATSIM, an unnual event taking place in the virtual skies above Denmark.">
+	<meta name="author" content="Copenhagen Live {{ date("Y") }}">
+		
+	<!-- Core CSS -->
+	<link href="{{ asset('/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('/assets/css/bootstrap-timepicker.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('/assets/css/main.css') }}" rel="stylesheet">
+		
+	<!-- AddOn/Plugin CSS -->
+	<link href="{{ asset('/assets/css/green.css') }}" rel="stylesheet" title="Color">
+	<link href="{{ asset('/assets/css/owl.carousel.css') }}" rel="stylesheet">
+	<link href="{{ asset('/assets/css/owl.transitions.css') }}" rel="stylesheet">
+	<link href="{{ asset('/assets/css/animate.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('/assets/css/aos.css') }}" rel="stylesheet">
+		
+	<!-- Custom CSS -->
+	<link href="{{ asset('/assets/css/custom.css') }}" rel="stylesheet">
+		
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Lato:400,900,300,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic" rel="stylesheet">
+		
+	<!-- Icons/Glyphs -->
+	<link href="{{ asset('/assets/fonts/fontello.css') }}" rel="stylesheet">
+		
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="{{ asset('/assets/images/favicon.ico') }}">
+		
+	<!-- HTML5 elements and media queries Support for IE8 : HTML5 shim and Respond.js -->
+	<!--[if lt IE 9]>
+		<script src="assets/js/html5shiv.js"></script>
+		<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+	<title>Copenhagen Live {{ date("Y") }}</title>
